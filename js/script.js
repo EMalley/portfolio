17486102b18b22be26ad4dcf9fca7a8644323638
Eric Malley <emalley998@gmail.com>
2023-08-20 12:11:32 -0400

    typing javascript feature added. Completed Header Section

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function runTypingEffect() {
+  const text = 'I Am Eric Malley'
+  const typingElement = document.getElementById('typing-text')
+  const typingDelay = 150 // delay between each typing in ms
+
+  typeText(text, typingElement, 0, typingDelay)
+}
+
+function typeText(text, typingElement, index, delay) {
+  if (index < text.length) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(index)
+      typeText(text, typingElement, index + 1, delay)
+    }, delay)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect)
